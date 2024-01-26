8619c8a111ee7f967869c86489aade18151d38fe
Juhyuns0n <juhyunson@ewhain.net>
2024-01-26 20:18:18 +0900

    기록하기 레이아웃

--- app/src/main/java/com/example/eat/RecordFragment.kt
+++ app/src/main/java/com/example/eat/RecordFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.example.eat.databinding.RecordFragmentBinding
 
 class RecordFragment: Fragment() {
+    private var _binding: RecordFragmentBinding?=null
+    private val binding get()=_binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.record_fragment,container,false)
+        _binding=RecordFragmentBinding.inflate(inflater,container,false)
+        binding.spinnerMainCategory.adapter=ArrayAdapter.createFromResource(requireContext(),R.array.main_category,R.layout.custom_spinner_dropdown_item)
+        binding.spinnerUnit.adapter=ArrayAdapter.createFromResource(requireContext(),R.array.unit,android.R.layout.simple_list_item_1)
+        return binding.root
     }
+
 }
